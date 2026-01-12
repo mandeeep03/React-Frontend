26651aed8952e222389b1cfc2588ead4ba9951f5
mandeeep03 <mandeepbhatt52@gmail.com>
2026-01-12 19:30:16 +0530

    done something

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,14 +1,14 @@
 
 function MovieCard({movie}){
     function onFavoriteClick(){
-        alert("clicked")
+      
     }
     return (
       <div className="w-[16rem] bg-zinc-800 hover:bg-zinc-900 p-4 border-1 rounded-xl text-white">
         <div className="relative">
           <img src={movie.poster_url} alt={movie.title} />
           <button className="absolute top-2  right-2" onClick={onFavoriteClick}>
-          ♡
+            ♡♥️
           </button>
           <div className=""></div>
         </div>
